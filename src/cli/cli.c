4fba2441d6ce36cb7518d7006cb4fe0aeafebbd2
casept <davids.paskevics@gmail.com>
2022-10-22 20:46:47 +0200

    Make choosing key length more intuitive

--- src/cli/cli.c
+++ src/cli/cli.c
@@ -1,5 +1,6 @@
 #include "cli.h"
 
+#include <libusb.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -29,39 +30,55 @@ CLIConfig cli_parse(const int argc, const char* argv[]) {
     // Get arguments for programming
     if (conf.action == CLI_ACTION_PROGRAM) {
         // We need everything to be specified
-        if (argc != 5) {
-            conf.action = CLI_ACTION_INVALID;
-            return conf;
+        // If not, assume the default key length (6 bytes)
+        bool have_explicit_length = false;
+        conf.is_8_char_code = false;
+        switch (argc) {
+            case 4:
+                have_explicit_length = false;
+                break;
+            case 5:
+                have_explicit_length = true;
+                break;
+            default:
+                conf.action = CLI_ACTION_INVALID;
+                return conf;
+        }
+
+        size_t arg_offset = 2;
+        if (have_explicit_length) {
+            // Check whether a key length has been explictly set.
+            if (strncmp(argv[arg_offset], "-6", strlen("-6")) == 0) {
+                conf.is_8_char_code = false;
+            } else if (strncmp(argv[arg_offset], "-8", strlen("-8")) == 0) {
+                conf.is_8_char_code = true;
+            } else {
+                conf.action = CLI_ACTION_INVALID;
+                return conf;
+            }
+            // Other arguments shift over by 1 if key length is passed
+            arg_offset++;
         }
 
         // Obtain serial
-        if (strlen(argv[2]) != HYPERHOTP_SERIAL_LEN) {
+        if (strlen(argv[arg_offset]) != HYPERHOTP_SERIAL_LEN) {
             conf.action = CLI_ACTION_INVALID;
         }
-        strncpy(conf.serial, argv[2], HYPERHOTP_SERIAL_LEN);
+        strncpy(conf.serial, argv[arg_offset], HYPERHOTP_SERIAL_LEN);
+        arg_offset++;
 
         // Obtain seed
-        if (strlen(argv[3]) != HYPERHOTP_SEED_LEN_ASCII) {
+        if (strlen(argv[arg_offset]) != HYPERHOTP_SEED_LEN_ASCII) {
             conf.action = CLI_ACTION_INVALID;
         }
-        strncpy(conf.seed, argv[3], HYPERHOTP_SEED_LEN_ASCII);
-
-        // Obtain code length
-        if (strncmp(argv[4], "false", strlen("false")) == 0) {
-            conf.is_8_char_code = false;
-        } else if (strncmp(argv[4], "true", strlen("true")) == 0) {
-            conf.is_8_char_code = true;
-        } else {
-            conf.action = CLI_ACTION_INVALID;
-        }
+        strncpy(conf.seed, argv[arg_offset], HYPERHOTP_SEED_LEN_ASCII);
+        arg_offset++;
     }
 
     return conf;
 }
 
 void cli_print_help(const char* binary_path) {
-    fprintf(stderr,
-            "Usage: %s [help|check|reset|program] <8-character serial number> <40-character hex seed> <whether to "
-            "generate 6-character (false) or 8-character (true) HOTP codes>\n",
+    fprintf(stderr, "Usage: %s [help|check|reset|program] [-68] <8-character serial number> <40-character hex seed>\n",
             binary_path);
 }
